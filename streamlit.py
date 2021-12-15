542376869d2193566ad8d4e808e9795c1cc198d6
Anisha <anisha.basil@stonybrook.edu>
2021-12-15 15:45:20 -0500

    .

--- streamlit.py
+++ streamlit.py
@@ -6,6 +6,7 @@ Created on Wed Dec 15 15:18:42 2021
 @author: anishabasil
 """
 
+import pandas as pd
 import plotly.express as px
 import pandas as pd
 import streamlit as st
@@ -13,3 +14,7 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+
+def load_hospitals(allow_input_mutation=True):
+    df_hospital_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
+    return df_hospital_2
