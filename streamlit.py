449993eb216347456e0f3fc0c83cb5657dcd7404
Anisha <anisha.basil@stonybrook.edu>
2021-12-15 16:16:17 -0500

    .

--- streamlit.py
+++ streamlit.py
@@ -33,3 +33,12 @@ print ('inpatient_info:' , len(inpatientdf))
 
 st.title('STREAMLIT APP DEPLOYMENT')
 st.write('Welcome, *Everyone!* :sunglasses:')
+
+#LOADER BAR TO STIMULATE LOADING    
+my_bar = st.progress(0)
+for percent_complete in range(100):
+     time.sleep(0.1)
+     my_bar.progress(percent_complete + 1)
+
+color = st.color_picker('Pick A Color', '#00f900')
+st.write('The current color is', color)
